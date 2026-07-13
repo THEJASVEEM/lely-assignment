ff74ce7aef51e3525c56bf6519d77c842ddae4b6
thejasveem <thejasveem7@gmail.com>
2026-07-13 10:40:21 +0530

    Initialize Flutter Project

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LelyAssignmentApp());
+}
+
+class LelyAssignmentApp extends StatelessWidget {
+  const LelyAssignmentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lely Assignment',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Lely Assignment',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+          ),
+        ),
+      ),
+    );
+  }
+}
